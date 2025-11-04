34f75f3ebe6d4c1dc15939d1dd7047b4168963a7
Edgar <jes25mm5@gmail.com>
2025-11-04 13:36:12 -0600

    lambda, sqrt, expt, tail

--- Interp.hs
+++ Interp.hs
@@ -6,20 +6,22 @@ import Desugar -- Importamos el módulo con el ASA
 data ASAV = NumV Int
               | BooleanV Bool
               | PairV ASAV ASAV
+              | ConsV ASAV ASAV   -- agregado para listas
+              | NilV              -- agregado para lista vacía
               | Closure String ASA ENV
               | Error String
               | AppV ASAV ASA
               deriving (Show)
 
-    -- Tipo de dato ENV que representa a los ambientes léxicos.
-    -- Un ambiente léxico se puede representar como una lista de
-    -- tuplas de identificadores (String) con un valor canónico.
+-- Tipo de dato ENV que representa a los ambientes léxicos.
+-- Un ambiente léxico se puede representar como una lista de
+-- tuplas de identificadores (String) con un valor canónico.
 type ENV = [(String, ASAV)]
 
-    -- Configuraciones (tuplas) de ASA, ENV
+-- Configuraciones (tuplas) de ASA, ENV
 type Config = (ASA, ENV)
 
-    -- Función de transición de paso pequeño (small step)
+-- Función de transición de paso pequeño (small step)
 smallStep :: Config -> Config
 smallStep (Nil, env) = (Nil, env)                                                 -- Nil-Rule
 smallStep ((Id x), env) =                                                         -- Lookup-Hit
@@ -30,106 +32,118 @@ smallStep ((Boolean b), env) = ((Boolean b), env)                               
 smallStep ((Add (Num n) (Num m)), env) = (Num (n + m), env)                    -- Add-Num
 smallStep ((Add (Num n) e2), env) = 
                                     let (e2', env') = smallStep (e2, env)
-                                    in (Add (Num n) e2', env)                       -- Add-Right
+                                    in (Add (Num n) e2', env')                       -- Add-Right
 smallStep ((Add e1 e2), env) = 
                             let (e1', env') = smallStep (e1, env)          
-                            in ((Add e1' e2), env)                                   -- Add-Left
+                            in ((Add e1' e2), env')                                   -- Add-Left
 smallStep ((Sub (Num n) (Num m)), env) = (Num (n - m), env)                    -- Sub-Num
 smallStep ((Sub (Num n) e2), env) = 
                                 let (e2', env') = smallStep (e2, env)
-                                in (Sub (Num n) e2', env)                           -- Sub-Right
+                                in (Sub (Num n) e2', env')                           -- Sub-Right
 smallStep ((Sub e1 e2), env) = 
                             let (e1', env') = smallStep (e1, env)          
-                            in ((Sub e1' e2), env)                                   -- Sub-Left
+                            in ((Sub e1' e2), env')                                   -- Sub-Left
 smallStep ((Mult (Num n) (Num m)), env) = (Num (n * m), env)                   -- Mult-Num
 smallStep ((Mult (Num n) e2), env) = 
                                     let (e2', env') = smallStep (e2, env)
-                                    in (Mult (Num n) e2', env)                      -- Mult-Right
+                                    in (Mult (Num n) e2', env')                      -- Mult-Right
 smallStep ((Mult e1 e2), env) = 
                             let (e1', env') = smallStep (e1, env)          
-                            in ((Mult e1' e2), env)                                  -- Mult-Left
+                            in ((Mult e1' e2), env')                                  -- Mult-Left
 smallStep ((Div (Num n) (Num 0)), env) = error ("Error. División entre cero detectada")  -- Div-Cero
 smallStep ((Div (Num n) (Num m)), env) = (Num (div n m), env)                   -- Div-Num
 smallStep ((Div (Num n) e2), env) = 
                                     let (e2', env') = smallStep (e2, env)
-                                    in (Div (Num n) e2', env)                       -- Div-Right
+                                    in (Div (Num n) e2', env')                       -- Div-Right
 smallStep ((Div e1 e2), env) = 
                             let (e1', env') = smallStep (e1, env)          
-                            in ((Div e1' e2), env)                                   -- Div-Left
+                            in ((Div e1' e2), env')                                   -- Div-Left
 smallStep ((Eq (Num n) (Num m)), env) = (Boolean (n == m), env)                  -- Eq-Num
 smallStep ((Eq (Num n) e2), env) =                                               -- Eq-Right
                                     let (e2', env') = smallStep (e2, env)
-                                    in ((Eq (Num n) e2'), env)
+                                    in ((Eq (Num n) e2'), env')
 smallStep ((Eq e1 e2), env) =                                                   -- Eq-Left
                                     let (e1', env') = smallStep (e1, env)
-                                    in ((Eq e1' e2), env)
+                                    in ((Eq e1' e2), env')
 smallStep ((MayorQue (Num n) (Num m)), env) = (Boolean (n > m), env)             -- MayorQue-Num
 smallStep ((MayorQue (Num n) e2), env) =                                         -- MayorQue-Right
                                     let (e2', env') = smallStep (e2, env)
-                                    in ((MayorQue (Num n) e2'), env)
+                                    in ((MayorQue (Num n) e2'), env')
 smallStep ((MayorQue e1 e2), env) =                                               -- MayorQue-Left
                                     let (e1', env') = smallStep (e1, env)
-                                    in ((MayorQue e1' e2), env)
+                                    in ((MayorQue e1' e2), env')
 smallStep ((MenorQue (Num n) (Num m)), env) = (Boolean (n < m), env)             -- MenorQue-Num
 smallStep ((MenorQue (Num n) e2), env) =                                         -- MenorQue-Right
                                     let (e2', env') = smallStep (e2, env)
-                                    in ((MenorQue (Num n) e2'), env)
+                                    in ((MenorQue (Num n) e2'), env')
 smallStep ((MenorQue e1 e2), env) =                                               -- MenorQue-Left
                                     let (e1', env') = smallStep (e1, env)
-                                    in ((MenorQue e1' e2), env)
+                                    in ((MenorQue e1' e2), env')
 smallStep ((Not (Boolean False)), env) = (Boolean True, env)                     -- Not-False
 smallStep ((Not (Boolean True)), env) = (Boolean False, env)                     -- Not-True                   
 smallStep ((Not e), env) =                                                         -- Not-Step
                             let (e', env') = smallStep (e, env)
-                            in (Not e', env)
+                            in (Not e', env')
 smallStep ((Pair v1 v2), env)                                                      -- Pair-Val
                     | isCanon v1 && isCanon v2 = ((Pair v1 v2), env)                             
 smallStep ((Pair v e2), env)                                                       -- Pair-Right
                     | isCanon v =                                                
                         let (e2', env') = smallStep (e2, env)
-                        in (Pair v e2', env)
+                        in (Pair v e2', env')
 smallStep ((Pair e1 e2), env) =                                                    -- Pair-Left
                             let (e1', env') = smallStep (e1, env)
-                            in (Pair e1' e2, env)
+                            in (Pair e1' e2, env')
 smallStep ((If (Boolean True) t f), env) = (t, env)                               -- If-True
 smallStep ((If (Boolean False) t f), env) = (f, env)                              -- If-False
 smallStep ((If g t f), env) =                                                      -- If-Guard
                             let (g', env') = smallStep (g, env)
-                            in ((If g' t f), env)
+                            in ((If g' t f), env')
 smallStep ((Fst (Pair v1 v2)), env) = (v1, env)                                   -- Fst-Pair
 smallStep ((Fst v), env)                                                           -- Fst-Err
                 | ((isCanon v) && (isPair v) == False) = error ("Error. La operacion fst solo funciona con pares")
 smallStep ((Fst e), env) =                                                         -- Fst-Arg
                             let (e', env') = smallStep (e, env)
-                            in (Fst e', env)
+                            in (Fst e', env')
 smallStep ((Snd (Pair v1 v2)), env) = (v2, env)                                   -- Snd-Pair
 smallStep ((Snd v), env)                                                           -- Snd-Err
                 | ((isCanon v) && (isPair v) == False) = error ("Error. La operacion snd solo funciona con pares") 
 smallStep ((Snd e), env) =                                                         -- Snd-Arg
                         let (e', env') = smallStep (e, env)
-                        in (Snd e', env)
+                        in (Snd e', env')
 smallStep ((Cons v1 v2), env)                                                      -- Cons-Val
                     | isCanon v1 && isCanon v2 = ((Cons v1 v2), env)                             
 smallStep ((Cons v e2), env)                                                       -- Cons-Right
                     | isCanon v =                                                
                             let (e2', env') = smallStep (e2, env)
-                            in (Cons v e2', env)
+                            in (Cons v e2', env')
 smallStep ((Cons e1 e2), env) =                                                    -- Cons-Left
                             let (e1', env') = smallStep (e1, env)
-                            in (Cons e1' e2, env)
+                            in (Cons e1' e2, env')
 smallStep ((Head (Cons e1 e2)), env) = (e1, env)                                  -- Head-Cons
 smallStep ((Head e), env) =                                                       -- Head-Arg
                             let (e', env') = smallStep (e, env)
-                            in (Head e', env)
+                            in (Head e', env')
 smallStep ((Tail (Cons e1 e2)), env) = (e2, env)                                  -- Tail-Cons
 smallStep ((Tail e), env) =                                                       -- Tail-Arg
                             let (e', env') = smallStep (e, env)
-                            in (Tail e', env)
+                            in (Tail e', env')
+-- Primitivas sqrt y expt
+smallStep ((App (Id "sqrt") (Num n)), env) = (Num (floor (sqrt (fromIntegral n))), env)
+smallStep ((App (Id "sqrt") e2), env) =
+    let (e2', env') = smallStep (e2, env)
+    in (App (Id "sqrt") e2', env')
+smallStep ((App (App (Id "expt") (Num n)) (Num m)), env) = (Num (n ^ m), env)
+smallStep ((App (App (Id "expt") (Num n)) e2), env) =
+    let (e2', env') = smallStep (e2, env)
+    in (App (App (Id "expt") (Num n)) e2', env')
+smallStep ((App (Id "expt") e1), env) =
+    let (e1', env') = smallStep (e1, env)
+    in (App (Id "expt") e1', env')
 smallStep ((App (Fun p c) e2), env) =                                           -- App-Bv y App-Arg
                             doClosure (appArg (funToClosure ((App (Fun p c) e2), env)))
 smallStep ((App e1 e2), env) =                                                  -- App-Fun
                             let (e1', env') = smallStep (e1, env)
-                            in ((App e1' e2), env)
+                            in ((App e1' e2), env')
 smallStep ((Fun p c), env) = ((Fun p c), env)
 
 -- Tipo de dato de configuraciones de ASAV, ENV
@@ -144,7 +158,7 @@ appArg :: ConfigASAV -> ConfigASAV
 appArg ((AppV (Closure p c env0) a), env)
                                     | (isCanon a) == False = 
                                         let (a', env') = smallStep (a, env)
-                                        in ((AppV (Closure p c env0)) a', env)
+                                        in ((AppV (Closure p c env0)) a', env')  -- fix env'
                                     | otherwise = ((AppV (Closure p c env0)) a, env)
 
 -- Funcion de transición de paso pequeño de una aplicación de closure a su beta reducción
@@ -158,6 +172,8 @@ isCanon (Num _) = True
 isCanon (Boolean _) = True
 isCanon (Pair _ _) = True
 isCanon (Fun _ _) = True
+isCanon (Cons _ _) = True     -- agregado para listas
+isCanon Nil = True            -- agregado para lista vacía
 isCanon _ = False
 
 -- Funcion auxiliar que verifica si un ASA es un Par
@@ -171,12 +187,18 @@ toASAV (Num n) = (NumV n)
 toASAV (Boolean b) = (BooleanV b)
 toASAV (Pair l r) = (PairV (toASAV(l)) (toASAV(r)))
 toASAV (Fun x c) = Closure x c []  -- Aquí, capturamos el closure con el ambiente vacío
+toASAV (Cons l r) = (ConsV (toASAV l) (toASAV r))   -- agregado
+toASAV Nil = NilV   -- agregado
+
+toASAV _ = Error "No se puede convertir ASA a ASAV"
 
 -- Funcion que transforma un ASAV en su forma equivalente en ASA.    
 returnASA :: ASAV -> ASA
 returnASA (NumV n) = (Num n)
 returnASA (BooleanV b) = (Boolean b)
 returnASA (PairV l r) = (Pair (returnASA(l)) (returnASA(r)))
+returnASA (ConsV l r) = (Cons (returnASA l) (returnASA r))   -- agregado
+returnASA NilV = Nil  -- agregado
 returnASA (Closure x c env) = Fun x c
 returnASA (Error msg) = error msg 
 
@@ -187,14 +209,12 @@ eval (e, env)
     | otherwise = eval (smallStep (e, env))
 
 -- Funcion que evalúa configuraciones usando eval y devuelve un resultado ASAV
-evalFinal :: Config -> ConfigASAV
+evalFinal :: Config -> (ASAV, ENV)
 evalFinal (e , env) = (case eval (e, env) of
                         (valor, env) -> (toASAV(valor), env))
-
 
 -- Funcion que busca el valor de un identificador en un ambiente y lo devuelve
 lookupHit :: String -> ENV -> ASAV
 lookupHit i [] = Error ("Error LookUpMiss: El identificador " ++ i ++ " es una variable libre")
 lookupHit i ((x,v):xs) = if i == x then v else lookupHit i xs 
 
-    
